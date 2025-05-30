6ca757da610db409450a0044e544b2d53401e73a
Igor <samptv59@gmail.com>
2025-05-30 20:56:16 +0300

    stars fix

--- src/components/review/Review.jsx
+++ src/components/review/Review.jsx
@@ -1,33 +1,19 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Counter from '../counter/Counter';
+import Stars from './Stars';
 import { RATING_MIN, RATING_MAX } from '../../constants';
 import styles from './Review.module.css';
 
 const Review = ({ review }) => {
   if (!review) return null;
   
-  const renderStars = (rating) => {
-    return Array.from({ length: RATING_MAX }, (_, i) => (
-      <span 
-        key={i} 
-        className={classNames(styles.star, {
-          [styles.filled]: i < rating
-        })}
-      >
-        ★
-      </span>
-    ));
-  };
-  
   return (
     <div className={styles.review}>
       <div className={styles.header}>
         <h4 className={styles.author}>{review.user}</h4>
         <div className={styles.rating}>
-          <div className={styles.stars}>
-            {renderStars(review.rating)}
-          </div>
+          <Stars rating={review.rating} />
           <span className={styles.ratingValue}>({review.rating}/5)</span>
         </div>
       </div>
